d327abd92704d0e01cbed69a081c2db006cd4d14
yergozhin <erzhanghg@gmail.com>
2024-03-16 21:09:40 +0100

    package-lock.json package.json src/App.css src/pages/Main.jsx yarn.lock

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,11 @@
-//import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 //import { useDispatch } from "react-redux";
 //import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 //import { addPage } from "../actions/Action";
 
 export default function Home(){
-    //const pages = useSelector((state) => state.pages);
+    const pages = useSelector((state) => state.pages);
     //const dispatch = useDispatch();
     //const navigateTo = useNavigate();
 
@@ -37,22 +37,34 @@ export default function Home(){
     return (
         <div className="navigation-menu">
             <div className="table-menu">
-                <input value={company} onChange={handleCompanyChange} type="text" name="company" placeholder="Company Name"/>
-                <input value={location} onChange={handleLoctionChange} type="text" name="location" placeholder="Location"/>
-                <input value={experience} onChange={handleExperienceChange} type="text" name="experience" placeholder="Experience"/>
-                <input value={annualSalary} onChange={handleAnnualSalaryChange} type="text" name="annualSalary" placeholder="Annual Salary"/>
-                <input value={date} onChange={handleDateChange} type="text" name="date" placeholder="Date"/>
-    <input value={specialization} onChange={handleSpecializationChange} type="text" name="specialization" placeholder="Specialization"/>
+                <input value={company} onChange={handleCompanyChange} type="text" name="company" placeholder="Company Name" className="myInputs"/>
+                <input value={location} onChange={handleLoctionChange} type="text" name="location" placeholder="Location" className="myInputs"/>
+                <input value={experience} onChange={handleExperienceChange} type="text" name="experience" placeholder="Experience" className="myInputs"/>
+                <input value={annualSalary} onChange={handleAnnualSalaryChange} type="text" name="annualSalary" placeholder="Annual Salary" className="myInputs"/>
+                <input value={date} onChange={handleDateChange} type="text" name="date" placeholder="Date" className="myInputs"/>
+                <input value={specialization} onChange={handleSpecializationChange} type="text" name="specialization" placeholder="Specialization" className="myInputs"/>
                 <table>
-                    <tr> 
-                        <td>Column 1</td>
-                        <td>Column 2</td>
-                        <td>Column 3</td>
-                        <td>Column 4</td>
-                        <td>Column 5</td>
-                        <td>Column 6</td>
+                    <tr>  
+                        <td className="columns">Company</td>
+                        <td className="columns">Location</td>
+                        <td className="columns">Experience</td>
+                        <td className="columns">Annual Salary</td>
+                        <td className="columns">Date</td>
+                        <td className="columns">Specialization</td>
                     </tr>
                 </table>
+                <div class="scroll">
+                    {pages?.map((pages) => (
+                        <p key={pages.company} className="person">
+                            <p>{pages.location}</p>
+                            <p>{pages.experience}</p>
+                            <p>{pages.annualSalary}</p>
+                            <p>{pages.date}</p>
+                            <p>{pages.specialization}</p>
+                            </p>
+                            )
+                    )}
+                </div>
             </div>
         </div>
     );
