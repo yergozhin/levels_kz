f297059157ecb9d9bbd7def5363e4008b0358b36
yergozhin <erzhanghg@gmail.com>
2024-03-16 11:36:51 +0100

    package-lock.json package.json src/App.js src/pages/Main.jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './pages/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main/>
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
+      {/*<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +25,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+  </header>*/}
     </div>
   );
 }
